218b3c02878d62779e3fb655af4aa63afc8f625f
bobostudio <m18576493832@163.com>
2022-10-09 14:18:58 +0800

    fix: udpate publish ci

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 npm config get registry
-npm config set registry=http://registry.npmjs.org
+npm config set registry=https://registry.npmjs.org
 echo '登录 npmjs '
 npm login
 echo '----------发布--------'
 npm pubilsh
 echo "发布完成"
-exit
